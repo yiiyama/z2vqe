2aad3a5acc4c7686953a052a1b5b1f55113c8a5f
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2025-05-07 17:14:57 +0900

    Added comments and docstrings

--- src/z2_lgt.py
+++ src/z2_lgt.py
@@ -7,12 +7,20 @@ from qiskit.quantum_info import SparsePauliOp, Pauli
 
 
 def calculate_num_params(num_sites, num_layers, trans_inv):
+    """Calculate the number of parameters for the given system configuration.
+
+    Copied from the VQE notebook.
+    """
     if trans_inv:
         return 5 * num_layers
     return 3 * num_layers * num_sites
 
 
 def initial_state(num_sites, boundary_cond):
+    """Return a QuantumCircuit corresponding to initial-state prep plus the number of qubits.
+
+    Copied from the VQE notebook.
+    """
     if boundary_cond == 'closed':
         num_qubits = 2 * num_sites
     else:
@@ -30,6 +38,7 @@ def initial_state(num_sites, boundary_cond):
 
 
 def initial_state_vector(num_sites):
+    """Compute the initial state as a state vector."""
     num_qubits = 2 * num_sites
     init_state = np.zeros((2,) * num_qubits)
     init_state[(0,) * num_qubits] = 1.
@@ -44,6 +53,10 @@ def initial_state_vector(num_sites):
 
 
 def xyz_gate(angle):
+    """Make a QuantumCircuit corresponding to the hopping term Hamiltonian unitary.
+
+    Copied from the VQE notebook.
+    """
     circuit = QuantumCircuit(3, name='xyz_gate')
     circuit.cx(2, 0)
     circuit.h(1)
@@ -61,6 +74,10 @@ def xyz_gate(angle):
 
 
 def z2_ansatz(num_sites, num_layers, num_params, boundary_cond, trans_inv):
+    """Make a QuantumCircuit corresponding to initial-state prep + ansatz unitary.
+
+    Copied from the VQE notebook and refactored with a layer-wise function call.
+    """
     circuit, _ = initial_state(num_sites, boundary_cond)
     theta = ParameterVector('theta', length=num_params)
 
@@ -75,6 +92,10 @@ def z2_ansatz(num_sites, num_layers, num_params, boundary_cond, trans_inv):
 
 
 def z2_ansatz_layer(num_sites, boundary_cond, params=None):
+    """Make a QuantumCircuit corresponding to a single ansatz layer unitary.
+
+    Copied from the VQE notebook.
+    """
     if boundary_cond == 'closed':
         num_qubits = 2 * num_sites
     else:
@@ -112,7 +133,11 @@ def z2_ansatz_layer(num_sites, boundary_cond, params=None):
 
 
 def create_hamiltonian(num_sites, j_hopping, f_gauge, mass, overall_coeff, boundary_cond,
-                       overall_coeff_cond):  # Qiskit Opflow
+                       overall_coeff_cond):
+    """Create a SparsePauliOp for the Hamiltonian.
+
+    Copied from the VQE notebook.
+    """
     num_qubits = 2 * num_sites if boundary_cond == 'closed' else 2 * num_sites - 1
 
     def op_positions(op1, pos1, op2=None, pos2=None, op3=None, pos3=None):
@@ -151,48 +176,97 @@ def create_hamiltonian(num_sites, j_hopping, f_gauge, mass, overall_coeff, bound
     return sum(ops).simplify()
 
 
-def get_symmetry_sector(state):
+def get_symmetry_sector(state: np.ndarray) -> tuple[np.ndarray, float]:
+    """Return the Gauss's law + U1 charge symmetry sector of a state vector.
+
+    The Gauss's law sector is given as an array of X[i-1,i]Z[i]X[i,i+1] eigenvalues. The U1 sector
+    is given as a mean of Z[i] eigenvalues.
+
+    Args:
+        state: State vector.
+
+    Returns:
+        A tuple of an array for the Gauss's law sector and a float for the U1 sector.
+    """
+
     num_qubits = np.log2(state.shape[0]).astype(int)
     num_sites = num_qubits // 2
-    state = state.reshape((2,) * (2 * num_sites))
+    # Reshape the state to make it applicable to single-qubit operations
+    state = state.reshape((2,) * num_qubits)
+    # Single-qubit Hadamard matrix
     hadamard = np.array([[1., 1.], [1., -1.]]) / np.sqrt(2.)
+    # Transform all link qubits to X basis
     for isite in range(num_sites):
+        # tensordot places the resulting qubit to axis 0, so we move it back to the original
+        # position with moveaxis
         state = np.moveaxis(np.tensordot(hadamard, state, [1, isite * 2 + 1]), 0, isite * 2 + 1)
 
+    # Compute the Z eigenvalue of each qubit
     zeigvals = np.empty(num_qubits, dtype=int)
     for iqubit in range(num_qubits):
+        # Move the qubit to axis 0 (just to make array indexing easier)
         moved = np.moveaxis(state, iqubit, 0)
+        # If the amplitude of moved[0] is 0, the state has only |1> for this qubit -> Z=-1
         if np.allclose(moved[0], 0.):
             zeigvals[iqubit] = -1
+        # If the amplitude of moved[1] is 0, the state has only |0> for this qubit -> Z=1
         elif np.allclose(moved[1], 0.):
             zeigvals[iqubit] = 1
         else:
             raise ValueError('Not a Gauss law eigenstate')
 
+    # gauss_sector[i] = zeigvals[2i-1] * zeigvals[2i] * zeigvals[2i+1]
     gauss_sector = np.roll(zeigvals, -1)[::2] * zeigvals[::2] * np.roll(zeigvals, 1)[::2]
+    # u1_sector[i] = sum_i [zeigvals[2i] / N]
     u1_sector = np.sum(zeigvals[::2]) / num_sites
     return gauss_sector, u1_sector
 
 
-def subspace_diagonalization(hamiltonian, gauss_sector, u1_sector):
+def subspace_diagonalization(
+    hamiltonian: SparsePauliOp,
+    gauss_sector: np.ndarray,
+    u1_sector: float
+) -> tuple[np.ndarray, np.ndarray]:
+    """Project the Hamiltonian to the given symmetry sector and diagonalize.
+
+    Args:
+        hamiltonian: Hamiltonian given as a SparsePauliOp.
+        gauss_sector: Array of XZX eigenvalues.
+        u1_sector: Mean of site Z eigenvalues.
+
+    Returns:
+        Arrays returned by np.linalg.eigh.
+    """
     num_qubits = hamiltonian.num_qubits
     num_sites = num_qubits // 2
 
+    # Enumerate all binary indices from 0 to 2^Nq - 1
+    # state_binary = [[0, ..., 0, 0], [0, ..., 0, 1], [0, ..., 1, 0], ..., [1, ..., 1, 1]]
     state_binary = (np.arange(2 ** num_qubits)[:, None] >> np.arange(num_qubits)[None, ::-1]) % 2
+    # Convert the binary indices to Z eigenvalues
     state_zeigvals = 1 - 2 * state_binary
+    # Compute the products of Z eigenvalues of neighboring three qubits
+    # state_gauss = [[1, 1, ..., 1, 1], [-1, 1, ..., -1, -1], [1, 1, ..., -1, -1], ...]
     state_gauss = (np.roll(state_zeigvals, -1, axis=1)[:, ::2]
                    * state_zeigvals[:, ::2]
                    * np.roll(state_zeigvals, 1, axis=1)[:, ::2])
-    state_u1 = np.sum(state_zeigvals[:, ::2], axis=1) / num_sites
+    # Compute the site (even qubit) sum of Z eigenvalues
+    # state_u1 = [Ns, Ns, Ns-2, ..., -Ns]
+    state_u1 = np.sum(state_zeigvals[:, ::2], axis=1)
+    # Extract the indices (symmetry subspace) that match the given sectors
     subspace = np.nonzero(
         np.all(np.equal(state_gauss, gauss_sector[None, :]), axis=1)
-        & np.equal(state_u1, u1_sector)
+        & np.equal(state_u1, np.round(u1_sector * num_sites).astype(int))
     )[0]
     subdim = subspace.shape[0]
 
-    # One-hot vectors
+    # Make the projectors onto the obtained subspace
+    # First create an array of one-hot vectors (s_0, s_1, ...) where s_i is a column vector with 1
+    # at subspace[i] and 0 elsewhere
     subbasis = np.zeros((2 ** num_qubits, subdim))
     subbasis[subspace, np.arange(subdim)] = 1.
+    # The current array corresponds to a projector in the Z basis. Since the subspace is defined in
+    # terms of X[link]Z[site]X[link] eigenstates, we transform the link qubits to X basis.
     subbasis = subbasis.reshape((2,) * num_qubits + (subdim,))
     hadamard = np.array([[1., 1.], [1., -1.]]) / np.sqrt(2.)
     for isite in range(num_sites):
@@ -201,9 +275,12 @@ def subspace_diagonalization(hamiltonian, gauss_sector, u1_sector):
             0,
             2 * isite + 1
         )
+    # Convert the projector array to CSR
     subbasis = scipy.sparse.csr_array(subbasis.reshape((2 ** num_qubits, subdim)))
 
+    # Project the Hamiltonian to the subspace
     block_hamiltonian = (subbasis.T @ (hamiltonian.to_matrix(sparse=True) @ subbasis)).todense()
+    # Clean up machine-epsilon entries
     block_hamiltonian = np.where(np.isclose(block_hamiltonian, 0.), 0., block_hamiltonian)
 
     return np.linalg.eigh(block_hamiltonian)
