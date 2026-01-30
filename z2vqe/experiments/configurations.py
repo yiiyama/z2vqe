54a454cc661efde5d24429432f05a9504f676cb5
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2026-01-30 15:31:14 +0900

    m_h is symmetric under Z2

--- z2vqe/experiments/configurations.py
+++ z2vqe/experiments/configurations.py
@@ -6,5 +6,6 @@ CONFIGS = {
     'g_m_hsp': (([0, 1], [2, 3], [4], [5]), dict(u1_charge=0, t2_momentum=0, cp_sign=1)),
     'gsp_m_h': (([0], [1], [2, 3], [4, 5]), dict(u1_charge=0, t2_momentum=0, p_sign=1)),
     'g_h': (([0, 1], [4, 5]), dict(u1_charge=0, c_phase=0, p_sign=1)),
-    'm_h': (([2, 3], [4, 5]), dict(u1_charge=0, c_phase=0, p_sign=1)),
+    'm_h': (([2, 3], [4, 5]), dict(u1_charge=0, z2_sign=1, c_phase=0, p_sign=1)),
+    'm_h_ref': (([2, 3], [4, 5]), dict(u1_charge=0, c_phase=0, p_sign=1)),
 }
